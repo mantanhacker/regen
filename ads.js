1fb8621eb04306f36fbbdfeb8bb8acbe6ae061b0
Mantan Hacker <dsulutaman@gmail.com>
2026-01-21 13:42:33 +0800

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,32 @@
+(function() {
+    const adUrl = 'https://supersedeaccolade.com/rqkgpc1c?key=de80fb2c5233f33a76803b5321103619';
+    const cookieName = 'pop_status';
+    const expireHours = 12;
+
+    function setAdCookie(name, value, hours) {
+        let date = new Date();
+        date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
+        document.cookie = name + "=" + value + ";expires=" + date.toUTCString() + ";path=/";
+    }
+
+    function getAdCookie(name) {
+        let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+        return match ? match[2] : null;
+    }
+
+    function executePop() {
+        if (getAdCookie(cookieName)) return;
+
+        const win = window.open(adUrl, '_blank');
+
+        if (win) {
+            win.blur();
+            window.focus();
+            setAdCookie(cookieName, 'true', expireHours);
+        }
+    }
+
+    document.addEventListener('click', function() {
+        executePop();
+    }, { once: true });
+})();
