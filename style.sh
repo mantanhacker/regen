1151fc09a048cecaf302d1fba23422c534bab8a0
Mantan Hacker <dsulutaman@gmail.com>
2026-02-06 15:17:13 +0800

    Update style.sh

--- style.sh
+++ style.sh
@@ -1,6 +1,6 @@
 script='<script src="/js/ads.js"></script>'
 
-for f in index.php detail_post.php; do
+for f in index.php detail_post.php result_lengkap.php detail.php; do
   grep -Fq "$script" "$f" && continue
 
   if grep -qi '</body>' "$f"; then
